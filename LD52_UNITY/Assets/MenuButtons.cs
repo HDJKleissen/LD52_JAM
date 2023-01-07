6e90f4bf574bf91e77800fa741cbe0bd2e5a984c
Kleissen,H.D.J. (Hilko) <h.d.j.kleissen@students.uu.nl>
2023-01-08 00:23:43 +0100

    Add menus

---
+++ LD52_UNITY/Assets/MenuButtons.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GotoScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
