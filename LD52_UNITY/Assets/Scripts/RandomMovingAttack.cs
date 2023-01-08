a48bf1cee980edb4bba7a1598d640ed1cd932a5b
Kleissen,H.D.J. (Hilko) <h.d.j.kleissen@students.uu.nl>
2023-01-08 20:28:21 +0100

    Combine spawn fix

--- LD52_UNITY/Assets/Scripts/RandomMovingAttack.cs
+++ LD52_UNITY/Assets/Scripts/RandomMovingAttack.cs
@@ -17,11 +17,11 @@ public class RandomMovingAttack : EnemyAttack
         Bounds bounds = controller.LevelBounds;
         bool vertical = Random.Range(0, 1f) > 0.5f;
         bool startPosOne = Random.Range(0, 1f) > 0.5f;
-        float xStart = vertical ? Random.Range(bounds.min.x, bounds.max.x) : startPosOne ? bounds.min.x : bounds.max.x;
-        float yStart = !vertical ? Random.Range(bounds.min.y, bounds.max.y) : startPosOne ? bounds.min.y : bounds.max.y;
+        float xStart = vertical ? Random.Range(bounds.min.x + sprite.sprite.bounds.size.x, bounds.max.x - sprite.sprite.bounds.size.x) : startPosOne ? bounds.min.x - sprite.sprite.bounds.size.x: bounds.max.x + sprite.sprite.bounds.size.x ;
+        float yStart = !vertical ? Random.Range(bounds.min.y + sprite.sprite.bounds.size.y, bounds.max.y - sprite.sprite.bounds.size.y) : startPosOne ? bounds.min.y - sprite.sprite.bounds.size.y  : bounds.max.y + sprite.sprite.bounds.size.y ;
         startPosition = new Vector2(xStart, yStart);
-        float xEnd = vertical ? xStart : startPosOne ? bounds.max.x : bounds.min.x;
-        float yEnd = !vertical ? yStart : startPosOne ? bounds.max.y : bounds.min.y;
+        float xEnd = vertical ? xStart : startPosOne ? bounds.max.x + sprite.sprite.bounds.size.x : bounds.min.x - sprite.sprite.bounds.size.x;
+        float yEnd = !vertical ? yStart : startPosOne ? bounds.max.y + sprite.sprite.bounds.size.y : bounds.min.y - sprite.sprite.bounds.size.y;
         endPosition = new Vector2(xEnd, yEnd);
 
         transform.SetPositionAndRotation(startPosition, Quaternion.Euler(0, 0, Mathf.Atan2((startPosition - endPosition).y, (startPosition - endPosition).x) * Mathf.Rad2Deg + 90f));
